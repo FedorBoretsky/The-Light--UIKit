c3c629d8898b9b338fde0305585b840eb9304693
Fedor Boretsky <fedor.boretsky@gmail.com>
2022-01-20 23:16:07 +0300

    Added support for camera's lightt.

--- The Light (UIKit)/ViewController.swift
+++ The Light (UIKit)/ViewController.swift
@@ -6,15 +6,23 @@
 //
 
 import UIKit
+import AVFoundation   // Camera light support.
 
 class ViewController: UIViewController {
+    
+    enum appModes {
+        case simpleLight
+        case trafficLights
+        case cameraLight
+    }
     
     // State values affect the interface.
     // Their change leads to an update.
     var state = (
-        mode: appModes.trafficLights,
+        mode: appModes.cameraLight,
         isSimpleLightOn: true,
-        trafficLightsCurrentIndex: 0
+        trafficLightsIndex: 0,
+        isCameraLightOn: false
     ) {
         didSet { updateUI() }
     }
@@ -29,6 +37,8 @@ class ViewController: UIViewController {
         updateUI()
     }
     
+    // MARK: - Interaction
+    
     @IBAction func tapScreen() {
         switch state.mode{
         case .simpleLight:
@@ -36,29 +46,24 @@ class ViewController: UIViewController {
             state.isSimpleLightOn.toggle()
         case .trafficLights:
             // Red-Yellow-Green cycle.
-            trafficLightsSwitchColor()
+            state.trafficLightsIndex = trafficLightsNewIndex()
         case .cameraLight:
-            break
+            state.isCameraLightOn.toggle()
         }
     }
+    
+    // MARK: - Update from state values
     
     func updateUI() {
         switch state.mode {
         case .simpleLight:
             view.backgroundColor = state.isSimpleLightOn ? .white : .black
         case .trafficLights:
-            view.backgroundColor = trafficLightsPalette[state.trafficLightsCurrentIndex]
+            view.backgroundColor = trafficLightsPalette[state.trafficLightsIndex]
         case .cameraLight:
             view.backgroundColor = .black
+            toggleTorch(on: state.isCameraLightOn)
         }
-    }
-    
-    // MARK: - Modes support
-    
-    enum appModes {
-        case simpleLight
-        case trafficLights
-        case cameraLight
     }
     
     // MARK: - Traffic lights support
@@ -67,9 +72,38 @@ class ViewController: UIViewController {
     let trafficLightsPalette = [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.9492518878, green: 0.9250498484, blue: 0.3488543158, alpha: 1), #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)]
     
     // Cycles through colors.
-    func trafficLightsSwitchColor() {
-        let nextIndex = state.trafficLightsCurrentIndex + 1
-        state.trafficLightsCurrentIndex = (nextIndex < trafficLightsPalette.count) ? nextIndex : 0
+    func trafficLightsNewIndex() -> Int {
+        let nextIndex = state.trafficLightsIndex + 1
+        return (nextIndex < trafficLightsPalette.count) ? nextIndex : 0
+    }
+    
+    // MARK: - Camera light support
+    
+    //
+    //
+    // Source of the code and details:
+    // https://www.hackingwithswift.com/example-code/media/how-to-turn-on-the-camera-flashlight-to-make-a-torch
+    //
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
     }
     
 }
