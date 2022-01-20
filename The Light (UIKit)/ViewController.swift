4533c0bb0ba05ac547b37e948a726eec904abd08
Fedor Boretsky <fedor.boretsky@gmail.com>
2022-01-20 17:14:21 +0300

    Cleaned the code a little.

--- The Light (UIKit)/ViewController.swift
+++ The Light (UIKit)/ViewController.swift
@@ -32,9 +32,11 @@ class ViewController: UIViewController {
     @IBAction func tapScreen() {
         switch state.mode{
         case .simpleLight:
+            // Black-white cycle.
             state.isSimpleLightOn.toggle()
         case .trafficLights:
-            trafficLightsSwitchToNext()
+            // Red-Yellow-Green cycle.
+            trafficLightsSwitchColor()
         case .cameraLight:
             break
         }
@@ -61,16 +63,13 @@ class ViewController: UIViewController {
     
     // MARK: - Traffic lights support
     
-    // Traffic light possible colors
+    // Traffic light possible colors.
     let trafficLightsPalette = [#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), #colorLiteral(red: 0.9492518878, green: 0.9250498484, blue: 0.3488543158, alpha: 1), #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)]
     
-    // Switch pointer to next color.
-    func trafficLightsSwitchToNext() {
-        if state.trafficLightsCurrentIndex < trafficLightsPalette.count - 1 {
-            state.trafficLightsCurrentIndex += 1
-        } else {
-            state.trafficLightsCurrentIndex = 0
-        }
+    // Cycles through colors.
+    func trafficLightsSwitchColor() {
+        let nextIndex = state.trafficLightsCurrentIndex + 1
+        state.trafficLightsCurrentIndex = (nextIndex < trafficLightsPalette.count) ? nextIndex : 0
     }
     
 }
