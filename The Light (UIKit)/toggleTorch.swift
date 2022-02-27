b03ae8fd7f96ad67638ef9c084fac3667f895d15
Fedor Boretsky <fedor.boretsky@gmail.com>
2022-02-27 22:48:37 +0300

    Some documentation.

--- The Light (UIKit)/toggleTorch.swift
+++ The Light (UIKit)/toggleTorch.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Fedor Boretskiy on 21.02.2022.
 //
+//  Switch on/off the camera flashlight.
+//
 //  Code pulled from Paul Hudson's article
 //  «How to turn on the camera flashlight to make a torch»
 //  https://www.hackingwithswift.com/example-code/media/how-to-turn-on-the-camera-flashlight-to-make-a-torch
@@ -11,6 +13,8 @@
 
 import AVFoundation
 
+/// Switch on/off the camera flashlight.
+/// - Parameter on: The value to control the flashlight state.
 func toggleTorch(on: Bool) {
     guard let device = AVCaptureDevice.default(for: .video) else { return }
 
